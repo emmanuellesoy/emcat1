fec4c39317b5d98e752dc915b341e361f0559897
shannonbit <emmanuelle.laguna@gmail.com>
2012-07-24 13:36:35 -0500

    Hola Charly

--- application/views/barra_de_herramientas.php
+++ application/views/barra_de_herramientas.php
@@ -1,5 +1,4 @@
 <ul>
-    <li><a href="<?=base_url(); ?>index.php/principal/"><label>INICIO</label></a></li>
     <li><a href="<?=base_url(); ?>index.php/principal/"><label>VENTAS</label></a></li>
     <li><a href="<?=base_url(); ?>index.php/principal/index/clientes"><label>CLIENTES</label></a></li>
     <li><a href="<?=base_url(); ?>index.php/principal/index/productos"><label>PRODUCTOS</label></a></li>
--- application/views/editar_cliente.php
+++ application/views/editar_cliente.php
@@ -1,78 +1,71 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
 <section class="editar_cliente">
     <form class="feditar_cliente">
         <table class="teditarcliente">
             <tr>
                 <td>
                      <label>NOMBRE:</label>
-                    <input type="text" id="nombre_cliente" name="nombre_cliente" autofocus/>
+                    <input type="text" id="nombre_cliente" name="nombre_cliente" value="<?=$datos_cliente['nombre']; ?>" autofocus/>
                 </td>    
             </tr>
             <tr>
                 <td>
                     <label>APELLIDO P:</label>
-                    <input type="text" id="apellido_p" name="apellido_p" />
+                    <input type="text" id="apellido_p" name="apellido_p" value="<?=$datos_cliente['apellido_p']; ?>" />
                 </td>
             </tr>    
             <tr>
                 <td>
                     <label>APELLIDO M:</label>    
-                    <input type="text" id="apellido_m" name="apellido_m" />
+                    <input type="text" id="apellido_m" name="apellido_m" value="<?=$datos_cliente['apellido_m']; ?>" />
                 </td>
             </tr>
             <tr>
                 <td> 
                     <label>CALLE Y NUMERO:</label>
-                    <input type="text" id="calle_numero" name="calle_numero" />
+                    <input type="text" id="calle_numero" name="calle_numero" value="<?=$datos_cliente['calle_numero']; ?>" />
                 </td>
             </tr>    
             <tr>    
                 <td>
                     <label>COLONIA:</label>
-                    <input type="text" id="colonia" name="colonia" />    
+                    <input type="text" id="colonia" name="colonia" value="<?=$datos_cliente['colonia']; ?>" />    
                 </td>
             </tr>
             <tr>   
                 <td>
                     <label>DELEGACIÓN O MUNICIPIO:</label>
-                    <input type="text" id="delegacion_municipio" name="delegacion_municipio" />
+                    <input type="text" id="delegacion_municipio" name="delegacion_municipio" value="<?=$datos_cliente['delegacion_municipio']; ?>" />
                 </td>
             </tr>    
             <tr>
                 <td>
                     <label>CÓDIGO POSTAL:</label>
-                    <input type="text" id="codigo_postal" name="codigo_postal" />
+                    <input type="text" id="codigo_postal" name="codigo_postal" value="<?=$datos_cliente['codigo_postal']; ?>" />
                 </td>
             </tr>
             
             <tr>
                 <td> 
                     <label>TÉLEFONO PARTICULAR:</label>
-                    <input type="tel" id="telefono_particular" name="telefono_particular" />
+                    <input type="tel" id="telefono_particular" name="telefono_particular" value="<?=$datos_cliente['telefono_p']; ?>" />
                 </td>
             </tr>
             <tr>    
                 <td>
                     <label>TÉLEFONO MÓVIL:</label>
-                    <input type="tel" id="telefono_movil" name="telefono_movil" />
+                    <input type="tel" id="telefono_movil" name="telefono_movil" value="<?=$datos_cliente['telefono_m']; ?>" />
                 </td>
             </tr>
             <tr>    
                 <td>
                     <label>CORREO-E:</label>
-                    <input type="email" id="correo_e" name="correo_e" />
+                    <input type="email" id="correo_e" name="correo_e" value="<?=$datos_cliente['correo_e']; ?>" />
                 </td>
             </tr> 
             <tr>
                 <td>
                     <label>RFC:</label>
-                    <input type="text" id="rfc" name="rfc" />
+                    <input type="text" id="rfc" name="rfc" value="<?=$datos_cliente['rfc']; ?>" />
                 </td>
             </tr>
             <tr>
